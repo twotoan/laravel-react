07490c449ae0b632a1e62f676f2245643c70f28a
Matthew Davies <matthew.davies@Revive.local>
2018-02-14 16:20:18 +0000

    Initial Commit!

---
+++ app/Exceptions/Handler.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Exceptions;
+
+use Exception;
+use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+
+class Handler extends ExceptionHandler
+{
+    /**
+     * A list of the exception types that are not reported.
+     *
+     * @var array
+     */
+    protected $dontReport = [
+        //
+    ];
+
+    /**
+     * A list of the inputs that are never flashed for validation exceptions.
+     *
+     * @var array
+     */
+    protected $dontFlash = [
+        'password',
+        'password_confirmation',
+    ];
+
+    /**
+     * @param Exception $exception
+     * @return mixed|void
+     * @throws Exception
+     */
+    public function report(Exception $exception)
+    {
+        parent::report($exception);
+    }
+
+    /**
+     * @param \Illuminate\Http\Request $request
+     * @param Exception $exception
+     * @return \Illuminate\Http\JsonResponse|\Symfony\Component\HttpFoundation\Response
+     */
+    public function render($request, Exception $exception)
+    {
+        if ($exception instanceof \Illuminate\Database\Eloquent\ModelNotFoundException)
+        {
+            return response()->json([
+                'message' => 'Resource not found'
+            ], 404);
+        }
+        return parent::render($request, $exception);
+    }
+}
---
+++ resources/assets/js/components/Main.js
@@ -0,0 +1,100 @@
+import React, {Component} from 'react';
+import ReactDOM from 'react-dom';
+import Product from './Product';
+import AddProduct from './AddProduct';
+
+/* Main Component */
+class Main extends Component {
+
+    constructor() {
+
+        super();
+
+        /* currentProduct keeps track of the product currently
+         * displayed */
+        this.state = {
+            products: [],
+            currentProduct: null
+        }
+        this.handleAddProduct = this.handleAddProduct.bind(this);
+    }
+
+    componentDidMount() {
+        fetch('/api/products')
+            .then(response => {
+                return response.json();
+            })
+            .then(products => {
+                //Fetched product is stored in the state
+                this.setState({products});
+            })
+    }
+
+    renderProducts() {
+        return this.state.products.map(product => {
+            return (
+                //this.handleClick() method is invoked onClick.
+                <li onClick={
+                    () => this.handleClick(product)} key={product.id}>
+                    {product.title}
+                </li>
+            );
+        })
+    }
+
+    handleClick(product) {
+        //handleClick is used to set the state
+        this.setState({currentProduct: product});
+
+    }
+
+    handleAddProduct(product) {
+
+        product.price = Number(product.price);
+        /*Fetch API for post request */
+        fetch('api/products/', {
+            method: 'post',
+            /* headers are important*/
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            },
+
+            body: JSON.stringify(product)
+        })
+            .then(response => {
+                return response.json();
+            })
+            .then(data => {
+                //update the state of products and currentProduct
+                this.setState((prevState) => ({
+                    products: prevState.products.concat(data),
+                    currentProduct: data
+                }))
+            })
+
+    }
+
+    render() {
+        return (
+            /* The extra divs are for the css styles */
+            <div>
+                <div>
+                    <h3> All products </h3>
+                    <ul>
+                        {this.renderProducts()}
+                    </ul>
+                </div>
+
+                <Product product={this.state.currentProduct}/>
+                <AddProduct onAdd={this.handleAddProduct}/>
+            </div>
+        );
+    }
+}
+
+export default Main;
+
+if (document.getElementById('root')) {
+    ReactDOM.render(<Main/>, document.getElementById('root'));
+}
